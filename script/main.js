6bea16ca8fdaadb5fee790170f201d5fcc06687a
asj <dkstmdwl0615@naver.com>
2022-06-09 11:39:14 +0900

    blur 작동방식 수정을 위한 html 구조 수정 및 js 코드 분석

--- script/main.js
+++ script/main.js
@@ -13,6 +13,7 @@ $(function(){
         todoInput(toLocal);
     });
 
+    // Works to do 목록에서 할일과 시간 삭제하는 기능
     console.log($(".delete-bt").length);
     $(".delete-bt").on({
         click: function(e){
@@ -29,6 +30,8 @@ $(function(){
     })
 
 });
+
+// 할일과 시간의 입력창이 비어있을 경우 경고 호출
 function checkForm(){
     if($("#todoText").val() === "") {
         alert("할일을 입력하세요");
@@ -41,6 +44,8 @@ function checkForm(){
         return false;
     }
 }
+
+
 function clearInput(){
     $("#todoText").val(null);
     $("#num").val(null);
@@ -63,6 +68,8 @@ function loadData(_storage){
     // console.log(data);
     updateList(data);
 };
+
+// 입력한 할일과 시간을 works to do 에 입력(갱신)
 function updateList(data){
     var htmlString = '';
     $.each(data, function (index, todo){
@@ -109,11 +116,11 @@ function clock(_storage){
     // 입력한 시간을 inputVal에 저장
     var inputVal = $(_storage[0].time).val();
     $("#example-timer").circletimer({
-        // 타이머 완료 후 작동하는 것
+        // 타이머 완료 후 (원이 완전히 없어졌을 때) 작동하는 것
         onComplete: function() {
             alert("Time is up!");
         },
-        // 타이머를 작동시키는 것?
+        // 남은 원의 면적을 측정하여 남은 시간을 표시
         onUpdate: function(elapsed) {
             $("#time-elapsed").html(Math.round(elapsed));
         },
